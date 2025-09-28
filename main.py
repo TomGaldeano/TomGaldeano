5549ea5fada260f19d9cf1d638d2008e9595857f
lordAstom <102704944+lordAstom@users.noreply.github.com>
2025-09-28 02:20:58 +0200

    html page done

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, Response
 from flask_bootstrap import Bootstrap
 import werkzeug.security
 from functools import wraps
 from flask_wtf.csrf import CSRFProtect
+import time
 
 
 def create_app():
@@ -24,6 +25,79 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/programacion/css')
+def prog_css():
+    return render_template('prog_css.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/programacion/html')
+def prog_html():
+    return render_template('prog_html.html')
+
+@app.route('/programacion/js')
+def prog_js():
+    return render_template('prog_js.html')
+
+@app.route('/programacion/php')
+def prog_php():
+    return render_template('prog_php.html')
+
+@app.route('/programacion/python')
+def prog_python():
+    return render_template('prog_python.html')
+
+@app.route('/programacion/sql')
+def prog_sql():
+    return render_template('prog_sql.html')
+
+@app.route('/programacion/java')
+def prog_java():
+    return render_template('prog_java.html')
+
+@app.route('/programacion/c')
+def prog_c():
+    return render_template('prog_c.html')
+
+@app.route('/programacion/bash')
+def prog_bash():
+    return render_template('prog_bash.html')
+
+@app.route('/programacion/git')
+def prog_git():
+    return render_template('prog_git.html')
+    
+@app.route('/projects/personal')
+def pj_personal():
+    return render_template('pj_personal.html')
+
+@app.route('/projects/personal/div_showcase')
+def pj_per_div():
+    return render_template('pj_per_div.html')
+
+@app.route('/projects/personal/built_in_api_showcase')
+def pj_per_built_in_api():
+    return render_template('pj_per_built_in_api.html')
+
+@app.route('/projects/games')
+def pj_games():
+    return render_template('pj_games.html')
+
+@app.route('/projects/ordered')
+def pj_ordered():
+    return render_template('pj_ordered.html')
+
+@app.route('/computer_science/linux')
+def cs_linux():
+    return render_template('cs_linux.html')
+
+@app.route("/stream")
+def stream():
+    def event_stream():
+        while True:
+            time.sleep(2)  # simulate new data every 2 sec
+            yield f"data: {time.strftime('%H:%M:%S')}\n\n"
+    return Response(event_stream(), mimetype="text/event-stream")
+
+if __name__ == '__main__':  
+    app.run(host='0.0.0.0', port=5001, debug=True)
+
+
