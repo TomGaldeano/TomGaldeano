560dbc12137c985acf018cd88d303592ef57eeec
lordAstom <102704944+lordAstom@users.noreply.github.com>
2025-09-28 10:45:06 +0200

    28-9-25

--- main.py
+++ main.py
@@ -27,63 +27,63 @@ def home():
 
 @app.route('/programacion/css')
 def prog_css():
-    return render_template('prog_css.html')
+    return render_template('prog/prog_css.html')
 
 @app.route('/programacion/html')
 def prog_html():
-    return render_template('prog_html.html')
+    return render_template('prog/prog_html.html')
 
 @app.route('/programacion/js')
 def prog_js():
-    return render_template('prog_js.html')
+    return render_template('prog/prog_js.html')
 
 @app.route('/programacion/php')
 def prog_php():
-    return render_template('prog_php.html')
+    return render_template('prog/prog_php.html')
 
 @app.route('/programacion/python')
 def prog_python():
-    return render_template('prog_python.html')
+    return render_template('prog/prog_python.html')
 
 @app.route('/programacion/sql')
 def prog_sql():
-    return render_template('prog_sql.html')
+    return render_template('prog/prog_sql.html')
 
 @app.route('/programacion/java')
 def prog_java():
-    return render_template('prog_java.html')
+    return render_template('prog/prog_java.html')
 
 @app.route('/programacion/c')
 def prog_c():
-    return render_template('prog_c.html')
+    return render_template('prog/prog_c.html')
 
 @app.route('/programacion/bash')
 def prog_bash():
-    return render_template('prog_bash.html')
+    return render_template('prog/prog_bash.html')
 
 @app.route('/programacion/git')
 def prog_git():
-    return render_template('prog_git.html')
+    return render_template('prog/prog_git.html')
     
 @app.route('/projects/personal')
 def pj_personal():
-    return render_template('pj_personal.html')
+    return render_template('projects/pj_personal.html')
 
 @app.route('/projects/personal/div_showcase')
 def pj_per_div():
-    return render_template('pj_per_div.html')
+    return render_template('projects/pj_per_div.html')
 
 @app.route('/projects/personal/built_in_api_showcase')
 def pj_per_built_in_api():
-    return render_template('pj_per_built_in_api.html')
+    return render_template('projects/pj_per_built_in_api.html')
 
 @app.route('/projects/games')
 def pj_games():
-    return render_template('pj_games.html')
+    return render_template('projects/pj_games.html')
 
 @app.route('/projects/ordered')
 def pj_ordered():
-    return render_template('pj_ordered.html')
+    return render_template('projects/pj_ordered.html')
 
 @app.route('/computer_science/linux')
 def cs_linux():
