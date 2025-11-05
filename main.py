eacf6e7bc8331944e5664845e3ac09c8ba046573
TomGaldeano <98astom@gmail.com>
2025-11-05 12:55:39 +0100

    clase 5-11-25

--- main.py
+++ main.py
@@ -25,46 +25,6 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/programacion/css')
-def prog_css():
-    return render_template('prog/prog_css.html')
-
-@app.route('/programacion/html')
-def prog_html():
-    return render_template('prog/prog_html.html')
-
-@app.route('/programacion/js')
-def prog_js():
-    return render_template('prog/prog_js.html')
-
-@app.route('/programacion/php')
-def prog_php():
-    return render_template('prog/prog_php.html')
-
-@app.route('/programacion/python')
-def prog_python():
-    return render_template('prog/prog_python.html')
-
-@app.route('/programacion/sql')
-def prog_sql():
-    return render_template('prog/prog_sql.html')
-
-@app.route('/programacion/java')
-def prog_java():
-    return render_template('prog/prog_java.html')
-
-@app.route('/programacion/c')
-def prog_c():
-    return render_template('prog/prog_c.html')
-
-@app.route('/programacion/bash')
-def prog_bash():
-    return render_template('prog/prog_bash.html')
-
-@app.route('/programacion/git')
-def prog_git():
-    return render_template('prog/prog_git.html')
-    
 @app.route('/projects/personal')
 def pj_personal():
     return render_template('projects/pj_personal.html')
@@ -84,18 +44,6 @@ def pj_games():
 @app.route('/projects/ordered')
 def pj_ordered():
     return render_template('projects/pj_ordered.html')
-
-@app.route('/computer_science/linux')
-def cs_linux():
-    return render_template('cs_linux.html')
-
-@app.route("/stream")
-def stream():
-    def event_stream():
-        while True:
-            time.sleep(2)  # simulate new data every 2 sec
-            yield f"data: {time.strftime('%H:%M:%S')}\n\n"
-    return Response(event_stream(), mimetype="text/event-stream")
 
 if __name__ == '__main__':  
     app.run(host='0.0.0.0', port=5001, debug=True)
--- static/js/prog_js.js
+++
@@ -1,4 +0,0 @@
-// window.alert("Welcome to the page");
-document.getElementById("output_innerhtml").innerHTML = "Writing into an HTML element, using innerHTML or innerText.";
-document.getElementById("output_innertext").innerText = "Writing into an alert box, using window.alert().";
-console.log("Hello console!"); // Writing into the browser console, using console.log().
