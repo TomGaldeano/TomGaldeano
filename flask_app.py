90c34daac6f52f8181e0e96d6772f3f01c8877e7
lordAstom <102704944+lordAstom@users.noreply.github.com>
2025-12-01 23:53:53 +0100

    1-12-25

--- flask_app.py
+++ flask_app.py
@@ -45,6 +45,10 @@ def pj_per_built_in_api():
 def pj_games():
     return render_template('projects/pj_games.html')
 
+@app.route('/projects/games/sudoku')
+def sudoku():
+    return render_template('games/sudoku.html')
+
 @app.route('/projects/games/tresenraya')
 def tresenraya():
     return render_template('games/tresenraya.html')
