8ad42441507c2af0a963c23529d9da80988b599d
TomGaldeano <98astom@gmail.com>
2026-02-17 12:19:54 +0100

    buscaminasbase

--- flask_app.py
+++ flask_app.py
@@ -53,6 +53,10 @@ def sudoku():
 def tresenraya():
     return render_template('games/tresenraya.html')
 
+@app.route('/projects/games/buscaminas')
+def buscaminas():
+    return render_template('games/buscaminas.html')
+
 @app.route('/projects/ordered')
 def pj_ordered():
     return render_template('projects/pj_ordered.html')
