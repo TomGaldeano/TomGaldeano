f76cfd73f6b69050d912b04403b6efb79496b787
lordAstom <102704944+lordAstom@users.noreply.github.com>
2026-05-16 19:07:07 +0200

    pantalla peque juegos  no

--- static/js/spaceInvaders.js
+++
@@ -1,9 +0,0 @@
-const CANVAS_WIDTH = 800;
-const CANVAS_HEIGHT = 600;
-const canvas = document.querySelector('#spaceCanvas');
-const ctx = canvas.getContext('2d');
-function draw(){
-    ctx.fillStyle = "red"
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-}
-draw()
